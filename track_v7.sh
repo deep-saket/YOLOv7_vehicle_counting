3916be9b3a3c1e4ab0d516501873555a45496570
deep-saket <saketmohanty10@gmail.com>
2023-06-22 19:54:26 +0530

    added links to weights

--- track_v7.sh
+++ track_v7.sh
@@ -1,3 +1,4 @@
 VIDEO_PATH=../../../cctv_highway/cctv052x2004080520x01694.avi
+YOLOV7_WRIGHTS=weights/yolov7-tiny.pt
 
-python track_v7.py --source $VIDEO_PATH  --yolo-weights weights/yolov7-tiny.pt --save-txt --count --show-vid --draw 
+python track_v7.py --source $VIDEO_PATH  --yolo-weights $YOLOV7_WRIGHTS --save-txt --count --show-vid --draw 
