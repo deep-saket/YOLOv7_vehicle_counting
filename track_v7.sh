7724e3ea879e3079d1816a1a1d1b3c1fbb730091
deep-saket <saketmohanty10@gmail.com>
2023-06-22 18:50:39 +0530

    adding run.sh

---
+++ track_v7.sh
@@ -0,0 +1,3 @@
+VIDEO_PATH=../../cctv_highway/cctv052x2004080520x01694.avi
+
+python track_v7.py --source $VIDEO_PATH  --yolo-weights weights/yolov7-tiny.pt --save-txt --count --show-vid --draw --classes 2 3 5 7
